a5ebb801bd8e7bbe1a870091ae79905ffc77a19f
Carlo de Wolf <cdewolf@redhat.com>
2012-01-25 13:00:56 +0100

    Shorten the bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,5 @@ set -x
 cd bootstrap
 mkdir -p target/bootstrap-classes
 javac -sourcepath src/main/java -d target/bootstrap-classes `find src/main/java -name \*.java`
-(cd target/bootstrap-classes && jar cvfe ../jboss-noxius-bootstrap-bootstrap.jar org.jboss.noxius.bootstrap.Bootstrap `find . -name *.class`)
+#(cd target/bootstrap-classes && jar cvfe ../jboss-noxius-bootstrap-bootstrap.jar org.jboss.noxius.bootstrap.Bootstrap `find . -name *.class`)
+jar cvfe target/jboss-noxius-bootstrap-bootstrap.jar org.jboss.noxius.bootstrap.Bootstrap -C target/bootstrap-classes/ .
